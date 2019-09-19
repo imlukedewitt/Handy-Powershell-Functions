e92d5e863f2706624739153d2b5529e8e26751db
Luke <dewitt-l@mssu.edu>
2019-09-19 12:35:01 -0500

    add INI parser

--- Setups.ps1
+++ Setups.ps1
@@ -25,3 +25,20 @@ function Get-UNCPath
     if ($Quotes) { $path = '"' + $path + '"' }
     return $path
 }
+
+function Get-IniConfig
+    {
+        # Parses the config file and returns an array of the desired settings section
+        # To use, define $config as such: $config = Get-Content [path to config.ini]
+
+        param
+        (
+            [Parameter(Position=0, Mandatory=$true)]
+            [String]$Section
+        )
+        
+        $startIndex = $config | Select-String -Pattern "\[$Section\]" | Select-Object -ExpandProperty LineNumber
+        $sectionLength = $config[$startIndex..$config.Length] | Select-String -Pattern "\[*\]" | Select-Object -First 1 -ExpandProperty LineNumber
+        if (!$sectionLength) { $sectionLength = $config.Length - $startIndex + 1}
+        return $config[$startIndex..($startindex + $sectionLength - 2)] | Where-Object { $_.trim() -ne "" -and $_.trim() -notmatch "^;" }
+    }
