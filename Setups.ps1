fb6e163dd82f75d65f954a35544f3620f8fb9c39
Luke <dewitt-l@mssu.edu>
2019-09-19 11:16:55 -0500

    can now self elevate scripts on a network path

--- Setups.ps1
+++ Setups.ps1
@@ -1,10 +1,27 @@
-function runAsAdmin
+function RunAsAdmin
 {
-    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-        if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-            Exit
-        }
+    # Usage:
+    # if (RunAsAdmin) { [do whatever] }
+
+    $adminaccess = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+    if ($adminaccess) { return $true}
+    $argList = @("-NoLogo -ExecutionPolicy Bypass -File $(Get-UNCPath -Quotes)" )
+    Start-Process Powershell.exe -Verb Runas -ArgumentList $argList
+    exit 0
+}
+
+function Get-UNCPath
+{
+    param ( [switch]$ScriptRoot, [switch]$Quotes )
+
+    $path = $script:MyInvocation.MyCommand.Path
+    if ($path.contains([io.path]::VolumeSeparatorChar))
+    {
+        $psDrive = Get-PSDrive -Name $path.Substring(0,1) -PSProvider FileSystem
+        if ($psDrive.DisplayRoot) { $path = $path.Replace($psDrive.Name + [io.path]::VolumeSeparatorChar, $psDrive.DisplayRoot) }
     }
+
+    if ($ScriptRoot) { $path = $path.Replace([io.path]::DirectorySeparatorChar + $script:MyInvocation.MyCommand.Name, "") }
+    if ($Quotes) { $path = '"' + $path + '"' }
+    return $path
 }
