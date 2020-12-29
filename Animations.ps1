46a95fe6dbb41a914e28b2de857f17e294dc2a31
imlukedewitt <45362944+imlukedewitt@users.noreply.github.com>
2020-12-29 17:52:30 -0600

    Update demonstration text

--- Animations.ps1
+++ Animations.ps1
@@ -135,7 +135,9 @@ function write-block
 }
 
 cursor -hide
-animation -scriptblock {sleep 2}
-writeString -color green -noNewLine "hello there "
-writeString -color red -noNewLine "LUCAS"
-writeString -color green " how are ya today buddy"
+animation -scriptblock {Start-Sleep 3}
+writeString -color green "A Reading from the Book of Armaments, Chapter 4, Verses 16 to 20:"
+""
+writeString -color yellow 'Then did he raise on high the Holy Hand Grenade of Antioch, saying, "Bless this, O Lord, that with it thou mayst blow thine enemies to tiny bits, in thy mercy." And the people did rejoice and did feast upon the lambs and toads and tree-sloths and fruit-bats and orangutans and breakfast cereals ... Now did the Lord say, "First thou pullest the Holy Pin. Then thou must count to three. Three shall be the number of the counting and the number of the counting shall be three. Four shalt thou not count, neither shalt thou count two, excepting that thou then proceedeth to three. Five is right out. Once the number three, being the number of the counting, be reached, then lobbest thou the Holy Hand Grenade in the direction of thine foe, who, being naughty in my sight, shall snuff it."'
+spinner -scriptblock {Start-Sleep 3}
+cursor -show
